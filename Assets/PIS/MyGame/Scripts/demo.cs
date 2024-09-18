79dda8e6798ac4fd9ab6dacface54846cf679af4
Huy <43417013+Nguyen-Khanh-Huy@users.noreply.github.com>
2024-09-18 16:23:01 +0700

    Ver1
    Ver1

---
+++ Assets/PIS/MyGame/Scripts/demo.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class demo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool a;
+    void Start()
+    {
+        Debug.Log(a);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
