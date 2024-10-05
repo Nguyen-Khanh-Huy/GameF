d03baf95a364d67d47f4524e8b9eebb6198ba93e
Huy <43417013+Nguyen-Khanh-Huy@users.noreply.github.com>
2024-10-05 09:45:30 +0700

    Free Moving Enemy
    Free Moving Enemy

--- Assets/PIS/MyGame/Scripts/FreeMovingEnemy.cs
+++ Assets/PIS/MyGame/Scripts/FreeMovingEnemy.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace PIS.PlatformGame
 {
@@ -15,11 +17,15 @@ namespace PIS.PlatformGame
 
         private bool _haveMovingPos; // Check: Co toa do di chuyen Den hay chua
         private Vector2 _movingPos; // toa do can di chuyen den
-
         protected override void Awake()
         {
             base.Awake();
             FSMInit(this);
+        }
+        protected override void Update()
+        {
+            base.Update();
+            GetTargetDir();
         }
         public void FindMaxMovePos()
         {// tim vi tri Max ma` Enemy di chuyen den
@@ -37,8 +43,86 @@ namespace PIS.PlatformGame
                 float randPosY = Random.Range(_movePosYD, _movePosYT);
                 _movingPos = new Vector2(randPosX, randPosY);
                 _movingDir = _movingPos - (Vector2)transform.position;
-                _movingPos.Normalize();
+                _movingDir.Normalize();
+                _movingDirBack = _movingDir;
+                _haveMovingPos = true;
             }
+            float angle = 0f;
+            if (canRotate)
+            {
+                angle = Mathf.Atan2(_movingDir.y, _movingDir.x) * Mathf.Rad2Deg;
+            }
+            if (_movingDir.x > 0)
+            {
+                if (canRotate)
+                {
+                    angle = Mathf.Clamp(angle, -41, 41);
+                    transform.rotation = Quaternion.Euler(0f, 0f, angle);
+                }
+                Flip(Diretion.Right);
+            } else if (_movingDir.x < 0)
+            {
+                if (canRotate)
+                {
+                    float newAngle = angle + 180f;
+                    newAngle = Mathf.Clamp(newAngle, 25, 325);
+                    transform.rotation = Quaternion.Euler(0f, 0f, newAngle);
+                }
+                Flip(Diretion.Left);
+            }
+            DestReachedChecking();
+        }
+        private void DestReachedChecking()
+        {
+            if(Vector2.Distance(transform.position, _movingPos) <= 0.5f)
+            {
+                _haveMovingPos = false;
+            }
+            else
+            {
+                _rb.velocity = _movingDir * _curSpeed;
+            }
+        }
+        //--------------------------------------------------------------------
+        #region FSM
+        protected override void Moving_Enter()
+        {
+            base.Moving_Enter();
+            FindMaxMovePos();
+            _haveMovingPos = false;
+        }
+        protected override void Chasing_Enter()
+        {
+            base.Chasing_Enter();
+            _movingDir = _targetDir;
+        }
+        protected override void GotHit_Update()
+        {
+            if (_isKnockBack)
+            {
+                //KnockBackMove(_targetDir.y);
+            }
+            else
+            {
+                _fsm.ChangeState(EnemyAnimState.Moving);
+            }
+        }
+
+        #endregion
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(transform.position, new Vector3(transform.position.x + movingDist, transform.position.y, transform.position.z));
+            
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(transform.position, new Vector3(transform.position.x - movingDist, transform.position.y, transform.position.z));
+
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y - movingDist, transform.position.z));
+
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y + movingDist, transform.position.z));
+
         }
     }
 
